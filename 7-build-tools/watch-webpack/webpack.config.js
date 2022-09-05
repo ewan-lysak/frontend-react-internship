2134f338bf1c8dbf1c2425eddb77670fd4b87d8e
Your Name <ewan.lysak@yandex.ru>
2022-09-05 12:35:27 +0300

    change-build

--- 7-build-tools/watch-webpack/webpack.config.js
+++ 7-build-tools/watch-webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './assets/index.js',
+  entry: './src/assets/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
