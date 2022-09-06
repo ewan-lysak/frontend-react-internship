53f0354848d9b36610a0066c623f278eecdb6437
Your Name <ewan.lysak@yandex.ru>
2022-09-06 16:40:51 +0300

    fix

--- 7-build-tools/watch-webpack/webpack.config.js
+++ 7-build-tools/watch-webpack/webpack.config.js
@@ -9,15 +9,22 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    rules: [ 
+    rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|svg|gif)$/,
-        use: ['file-loader']
-      },
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
+      }
     ],
   },
   resolve: {
@@ -28,6 +35,13 @@ module.exports = {
     }
   },
   plugins: [
-      new HtmlWebpackPlugin({ template: 'index.html' })
-    ]
-};
+    new HtmlWebPackPlugin({
+      template: "./src/template/pages/index.pug",
+      filename: "./index.html"
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/assets/images', to: './assets/images' },
+      { from: './src/assets/icons', to: './assets/fonts' },
+    ])
+  ]
+};     
