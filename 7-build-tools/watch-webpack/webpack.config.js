55c4b58b2899afb6815e6aed7165cb8ea7f9c6ea
Your Name <ewan.lysak@yandex.ru>
2022-09-08 17:54:19 +0300

    build-final

--- 7-build-tools/watch-webpack/webpack.config.js
+++ 7-build-tools/watch-webpack/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CopyWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
@@ -14,6 +14,7 @@ module.exports = {
   },
   module: {
     rules: [
+
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
@@ -31,17 +32,23 @@ module.exports = {
       }
     ],
   },
-  resolve: {
-    extensions: ['.js', '.json', '.png'],
-    alias: {
-      '@models': path.resolve(__dirname, 'src/images'),
-      '@': path.resolve(__dirname, 'src'),
-    }
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
       filename: "./index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/images'),
+          to: path.resolve(__dirname, 'dist/assets/images')
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons'),
+          to: path.resolve(__dirname, 'dist/assets/icons')
+        },
+      ]
     }),
   ]
 };     
